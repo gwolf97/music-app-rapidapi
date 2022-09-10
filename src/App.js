3be86e9d054aa5914b46ef740d58b6c0ca35a46e
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-09-10 16:24:46 -0500

    React, router, and redux setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Container} from '@mui/material';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container maxWidth="lg">
+      <Routes>     
+          <Route path="/" element={<HomeScreen/>}/>
+      </Routes>
+      </Container>
+    </Router>
   );
 }
 
