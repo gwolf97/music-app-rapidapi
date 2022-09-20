c72debb6f940055e53486e62f11f2141f64fd4d2
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-09-20 17:46:08 -0500

    Related songs component logs tracks

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getRelatedTracks } from '../actions/actions'
 
 const RelatedSongs = ({songKey}) => {
+
+    const dispatch = useDispatch()
+    const relatedSongs = useSelector(state => state.relatedTracks.songs)
+
+    React.useEffect(() => {
+        dispatch(getRelatedTracks(songKey))
+    }, [])
+
+    console.log(relatedSongs)
+
   return (
     <div style={{height:"100px"}}>RelatedSongs</div>
   )
