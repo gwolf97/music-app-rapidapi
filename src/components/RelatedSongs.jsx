550e9a99e54f11f0b602ffebc60bfd9bef62e38d
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-09-20 17:28:17 -0500

    Song screen mobile styles update

---
+++ src/components/RelatedSongs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RelatedSongs = () => {
+  return (
+    <div style={{height:"100px"}}>RelatedSongs</div>
+  )
+}
+
+export default RelatedSongs
--- src/screens/SongScreen.jsx
+++ src/screens/SongScreen.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux"
 import { getTrack } from "../actions/actions"
 import SongDetails from "../components/SongDetails"
+import RelatedSongs from '../components/RelatedSongs'
 
 const SongScreen = () => {
 
@@ -17,12 +18,17 @@ const SongScreen = () => {
 
   return (
     <>
-    <div className="song-details-div">
-            {!loading && success && (
-                            <SongDetails song={song}/>
-                        )}
+    <div className="song-screen-container" style={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+        <div className="song-details-div">
+                {!loading && success && (
+                                <SongDetails song={song}/>
+                            )}
+        </div>
+        <div className="related-songs-container" style={{marginTop:"250px"}}>
+             <RelatedSongs/>
+        </div>
     </div>
-                
+
     </>
   )
 }
